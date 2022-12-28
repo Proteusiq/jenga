252f8ff1013bcbc6feec9293a5835828b964f942
Prayson W. Daniel <prayson.wilfred@nttdata.com>
2022-12-28 14:12:28 +0100

    added condition to run

--- jenga.sh
+++ jenga.sh
@@ -5,15 +5,15 @@
 
 help() {
 
-cat << EOF  
+    cat <<EOF
 Usage: ./jenga --python=3.10 [-hrj]
 Unhelling Python Environments: install pyenv and poetry to manage environements
 
 -h, -help,          --help                  Display help
 
--p, -python,       --python                 Main Python version default `3.10`
+-p, -python,       --python                 Main Python version default $(3.10)
 
--r, -runcommands,  --runcommands            Run Commands default `.bashrc`
+-r, -runcommands,  --runcommands            Run Commands default $(.bashrc)
 
 -j, -jupyter,      --jupyter                Install a global Jupyter Lab
 
@@ -21,31 +21,28 @@ EOF
 
 }
 
-setup_pyenv(){
+setup_pyenv() {
     echo "Installing pyenv with global Python ${python} the rc is ${runcommands}"
     echo "Updating & Installing necessary packages"
 
-    sudo apt update -y && sudo apt install  -y curl git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev \
+    sudo apt update -y && sudo apt install -y curl git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev \
         zlib1g-dev libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl libffi-dev \
-        liblzma-dev;
-
+        liblzma-dev
 
     echo "Installing Pyenv"
-    # install pyenv 
+    # install pyenv
     # curl https://pyenv.run | bash
 
-    # export PYENV_ROOT="$HOME/.pyenv"
-    # export PATH="$PYENV_ROOT/bin:$PATH"
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
 
-    pyenv update && pyenv install ${python} && \
+    pyenv update && pyenv install ${python} &&
 
-    # set pyenv to .bashrc or .profile
-    echo -e '\nexport PYENV_ROOT="$HOME/.pyenv"
+        # set pyenv to .bashrc or .profile
+        echo -e '\nexport PYENV_ROOT="$HOME/.pyenv"
     \ncommand -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-    \neval "$(pyenv init -)"' >> ${runcommands};
+    \neval "$(pyenv init -)"' >>${runcommands}
 
-
-    
     #  source bashrc
     echo "Setting completed. To complete installation execute >> source ${runcommands}"
 }
@@ -54,49 +51,50 @@ export python=3.10
 export runcommands=$HOME/.bashrc
 export jupyter=0
 
-
 options=$(getopt -l "help,runcommands:,python:,jupyter" -o "hr:p:j" -a -- "$@")
 
 eval set -- "$options"
 
 check_args="true"
+run_setup="false"
 
-while true
-do
-case "$1" in
--h|--help) 
-    help
-    exit 0
-    ;;
--r|--runcommands) 
+while true; do
+    case "$1" in
+    -h | --help)
+        help
+        exit 0
+        ;;
+    -r | --runcommands)
+        shift
+        export runcommands="$HOME/.$1"
+        run_setup="true"
+        ;;
+    -p | --python)
+        shift
+        export python="$1"
+        run_setup="true"
+        ;;
+
+    -j | --jupyter)
+        export jupyter=1
+        ;;
+    --)
+
+        shift
+        break
+        ;;
+    esac
+
+    check_args="false"
     shift
-    export runcommands="$HOME/.$1"
-    ;;
--p|--python) 
-    shift
-    export python="$1"
-    ;;
-
--j|--jupyter)
-    export jupyter=1
-    ;;
---)
-
-
-    shift
-    break;;
-esac
-
-
-
-
-check_args="false"
-shift
 done
-[[ "$check_args" == "true" ]] && { echo "Run: ./jenga.sh --help"; exit 1;}
+[[ "$check_args" == "true" ]] && {
+    echo "Run: ./jenga.sh --help"
+    exit 1
+}
 
-setup_pyenv
-[[ "$jupyter" == 1 ]] && { echo "Install Jupyter Lab";}
+[[ "$run_setup" == "true" ]] && { setup_pyenv; }
 
+[[ "$jupyter" == 1 ]] && { echo "Install Jupyter Lab"; }
 
 echo $python $runcommands $jupyter
